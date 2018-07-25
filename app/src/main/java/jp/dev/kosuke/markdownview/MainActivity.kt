f3aa3224c054676346526ff43fb4ba780a1ed5fe
KosukeTakahashi <kousuke20010307@icloud.com>
2018-07-25 18:28:31 +0900

    revision

--- app/src/main/java/jp/dev/kosuke/markdownview/MainActivity.kt
+++ app/src/main/java/jp/dev/kosuke/markdownview/MainActivity.kt
@@ -64,12 +64,12 @@ class MainActivity : AppCompatActivity() {
             \\]
 
             ## Images
-            URL: https://commons.wikimedia.org/wiki/File:A300_Iran_Air_EP-IBT_THR_May_2010.jpg?uselang=ja
             ![img](https://upload.wikimedia.org/wikipedia/commons/9/9a/A300_Iran_Air_EP-IBT_THR_May_2010.jpg)
 
             URL: https://upload.wikimedia.org/wikipedia/commons/b/b5/Kotlin-logo.png
             ![img](https://upload.wikimedia.org/wikipedia/commons/b/b5/Kotlin-logo.png)
         """.trimIndent()
+        // URL: https://commons.wikimedia.org/wiki/File:A300_Iran_Air_EP-IBT_THR_May_2010.jpg?uselang=ja
 
         val listener = object: RendererListener {
             override fun onRenderStarted() {
